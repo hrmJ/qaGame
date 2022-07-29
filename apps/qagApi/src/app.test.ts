9d45d85d6066e643441e3646075e3b470d0dbebc
Juho Härme <juho.harme@gmail.com>
2022-07-29 09:08:11 +0300

    skaffold testing for api

---
+++ apps/qagApi/src/app.test.ts
@@ -0,0 +1,11 @@
+import { build } from "./app";
+import { describe, expect, it } from "vitest";
+
+// build()
+//
+
+describe("Name of the group", () => {
+  it("should ", () => {
+    expect(true).toBeTruthy();
+  });
+});
