b3757169379ce9aa6cf25003f297d524f1eb6bcb
Juho Härme <juho.harme@gmail.com>
2022-07-30 08:23:36 +0300

    add route for answers

--- apps/qagFront/src/__tests__/a.test.ts
+++ apps/qagFront/src/__tests__/a.test.ts
@@ -1,5 +1,29 @@
-import { describe, expect } from 'vitest';
+import { cleanup, render } from '@testing-library/svelte';
+import { describe, it } from 'vitest';
+import AnswerScreen from '../routes/a.svelte';
+import { server } from 'src/setupTests';
+import { rest } from 'msw';
 
 describe('answer screen', () => {
-	it('shows an answer on init', () => {});
+	afterEach(() => {
+		cleanup();
+	});
+
+	it('Shows a answer on init if request succesful ', async () => {
+		const text = 'text for a answer';
+		server.use(
+			rest.get('/cards/answer', (_, res, ctx) => res(ctx.status(200), ctx.json({ text })))
+		);
+		const { findByText } = render(AnswerScreen);
+		await findByText(text);
+	});
+
+	it('Shows an error message on init if request not succesful ', async () => {
+		const error = 'Vastauksen lataaminen ei onnistunut';
+		server.use(
+			rest.get('/cards/answer', (_, res, ctx) => res(ctx.status(400), ctx.json({ error: '' })))
+		);
+		const { findByText } = render(AnswerScreen);
+		await findByText(error);
+	});
 });
--- apps/qagFront/src/routes/a.svelte
+++ apps/qagFront/src/routes/a.svelte
@@ -1,4 +1,30 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	let cardLoadstate: Promise<Card> | null;
+	interface Card {
+		contentType: 'q' | 'a';
+		text: string;
+	}
+	const apiUrl = 'http://localhost:3000';
+	async function loadCard() {
+		const res = await fetch(`${apiUrl}/cards/answer`, {
+			headers: { 'Content-Type': 'application/json' }
+		}).catch(() => null);
+		if (!res?.ok) throw new Error('Vastauksen lataaminen ei onnistunut');
+		return await res.json();
+	}
+	onMount(() => {
+		cardLoadstate = loadCard();
+	});
+</script>
 
-describe('question screen', () => {
-	it('shows a question on init', () => {});
-});
+<article>
+	{#await cardLoadstate}
+		Ladataan vastausta
+	{:then card}
+		{card?.text ?? ''}
+	{:catch err}
+		{err.message}
+	{/await}
+</article>
+
